1979eb70041e9f842326521cd6cf96aee0c93410
Viero Fernando <60427892+vierofernando@users.noreply.github.com>
2021-11-11 15:51:20 +0700

    perf(getEmoji): use String.fromCharCode

--- src/utility/getEmoji.js
+++ src/utility/getEmoji.js
@@ -4,16 +4,9 @@
  * @returns {String} The emoji representation of the number
  */
 function getEmoji(number) {
-    if (number === 1) return "1️⃣";
-    else if (number === 2) return "2️⃣";
-    else if (number === 3) return "3️⃣";
-    else if (number === 4) return "4️⃣";
-    else if (number === 5) return "5️⃣";
-    else if (number === 6) return "6️⃣";
-    else if (number === 7) return "7️⃣";
-    else if (number === 8) return "8️⃣";
-    else if (number === 9) return "9️⃣";
-    else return number;
+	if (number > 9 || number < 1)
+		return number;
+	return String.fromCharCode(48 + number, 65039, 8419);
 }
 
 module.exports = getEmoji;
