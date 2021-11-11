5ecc4c010b8558283ca48c13781efdf44264e6f2
null <60427892+vierofernando@users.noreply.github.com>
2021-11-11 09:38:55 +0700

    fix: use the magic of null being dumb

--- src/utility/getNumber.js
+++ src/utility/getNumber.js
@@ -6,6 +6,7 @@
 function getNumber(emoji) {
     const e = [...emoji].map(x => x.charCodeAt());
     if (e[1] !== 65039 || e[2] !== 8419 || (e[0] < 48 || e[0] > 57))
+        return emoji;
     return e[0] - 48;
 }
 
